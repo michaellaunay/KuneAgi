b698f70a1c3f95be743299396b07f6db4ae3bf8b
Amen Souissi <amensouissi@ecreall.com>
2017-04-20 10:34:20 +0200

    fix messages, translations

--- novaideo/views/__init__.py
+++ novaideo/views/__init__.py
@@ -499,7 +499,7 @@ class NovaideoAPI(IndexManagementJsonView):
                 self.context, 'novaideoapi',
                 query={'op': previous_action + '_idea'})
             result['title'] = localizer.translate(
-                _('Oppose' if previous_action == 'oppose' else 'Support'))
+                _('Oppose') if previous_action == 'oppose' else _('Support'))
             result['withdraw'] = True
 
         user = get_current()
@@ -520,7 +520,7 @@ class NovaideoAPI(IndexManagementJsonView):
                 self.context, 'novaideoapi',
                 query={'op': previous_action + '_proposal'})
             result['title'] = localizer.translate(
-                _('Oppose' if previous_action == 'oppose' else 'Support'))
+                _('Oppose') if previous_action == 'oppose' else _('Support'))
             result['withdraw'] = True
 
         user = get_current()
