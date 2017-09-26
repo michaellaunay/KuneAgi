ecreall/nova-ideo

optimization: load tabs on-demand, update Chart js version, fix analytics view

novaideo/views/novaideo_view_manager/home.py
@@ -42,80 +42,91 @@ class ContentView(BasicView):
     wrapper_template = 'novaideo:views/templates/simple_wrapper.pt'
     content_type = 'idea'
     isactive = False
+    hasparent=True
+    filter_class = ''
+    view_filter = [('metadata_filter', ['states', 'keywords', 'challenges']), 'geographic_filter',
+                  'contribution_filter',
+                  ('temporal_filter', ['negation', 'created_date']),
+                  'text_filter', 'other_filter']
+    
+    def _get_validated(self, validated, user):
+        return validated
 
     def _add_filter(self, user):
         def source(**args):
             default_content = [self.content_type]
-            filter_ = {
-                'metadata_filter': {
-                    'content_types': default_content,
-                    'states': ['active', 'published']}
-            }
+            filter_ = self._get_validated({
+                'metadata_filter':
+                    {'content_types': default_content,
+                    'states': ['active', 'published']},
+            }, user)
             objects = find_entities(user=user, filters=[filter_], **args)
             return objects
 
         url = self.request.resource_url(
             self.context, '@@novaideoapi',
-            query={'view_content_type': self.content_type})
-        select = [('metadata_filter', ['states', 'keywords', 'challenges']), 'geographic_filter',
-                  'contribution_filter',
-                  ('temporal_filter', ['negation', 'created_date']),
-                  'text_filter', 'other_filter']
+            query={'view_content_id': self.content_id})
         return get_filter(
             self,
             url=url,
             source=source,
-            select=select,
+            select=self.view_filter,
             filter_source="home",
-            filterid=self.content_type)
+            filterid=self.viewid)
 
     def update(self):
-        user = get_current()
-        filter_form, filter_data = self._add_filter(user)
-        default_content = [self.content_type]
-        validated = {
-            'metadata_filter':
-                {'content_types': default_content,
-                'states': ['active', 'published']}
-        }
-        args = {}
-        args = merge_with_filter_view(self, args)
-        args['request'] = self.request
-        objects = find_entities(
-            user=user,
-            filters=[validated],
-            **args)
-        objects, sort_body = sort_view_objects(
-            self, objects, [self.content_type], user)
-        url = self.request.resource_url(
-            self.context, '',
-            query={'view_content_type': self.content_type})
-        batch = Batch(objects,
-                      self.request,
-                      url=url,
-                      default_size=BATCH_DEFAULT_SIZE)
-        self.title = _(self.title, mapping={'nb': batch.seqlen})
-        batch.target = "#results-" + self.content_type
-        filter_instance = getattr(self, 'filter_instance', None)
-        filter_body = None
-        if filter_instance:
-            filter_data['filter_message'] = self.title
-            filter_body = filter_instance.get_body(filter_data)
-        result_body, result = render_listing_objs(
-            self.request, batch, user)
-        values = {'bodies': result_body,
-                  'batch': batch,
-                  'empty_message': self.empty_message,
-                  'empty_icon': self.empty_icon,
-                  'filter_body': filter_body,
-                  'sort_body': sort_body}
-        if filter_form:
-            result = merge_dicts(
-                {'css_links': filter_form['css_links'],
-                 'js_links': filter_form['js_links']
-                }, result)
+        body = ''
+        result = {}
+        if self.isactive or self.params('on_demand') == 'load':
+            user = get_current()
+            filter_form, filter_data = self._add_filter(user)
+            default_content = [self.content_type]
+            validated = self._get_validated({
+                'metadata_filter':
+                    {'content_types': default_content,
+                    'states': ['active', 'published']},
+            }, user)
+            args = {}
+            args = merge_with_filter_view(self, args)
+            args['request'] = self.request
+            objects = find_entities(
+                user=user,
+                filters=[validated],
+                **args)
+            objects, sort_body = sort_view_objects(
+                self, objects, [self.content_type], user)
+            url = self.request.resource_url(
+                self.context, '',
+                query={'view_content_id': self.content_id})
+            batch = Batch(objects,
+                          self.request,
+                          url=url,
+                          default_size=BATCH_DEFAULT_SIZE)
+            self.title = _(self.title, mapping={'nb': batch.seqlen})
+            batch.target = "#results-" + self.content_id
+            filter_instance = getattr(self, 'filter_instance', None)
+            filter_body = None
+            if filter_instance:
+                filter_data['filter_message'] = self.title
+                filter_body = filter_instance.get_body(filter_data)
+            result_body, result = render_listing_objs(
+                self.request, batch, user)
+            values = {'bodies': result_body,
+                      'batch': batch,
+                      'empty_message': self.empty_message,
+                      'empty_icon': self.empty_icon,
+                      'filter_body': filter_body,
+                      'filter_class': self.filter_class,
+                      'sort_body': sort_body,
+                      'view': self}
+            if filter_form:
+                result = merge_dicts(
+                    {'css_links': filter_form['css_links'],
+                     'js_links': filter_form['js_links']
+                    }, result)
 
-        body = self.content(args=values, template=self.template)['body']
+            body = self.content(args=values, template=self.template)['body']
+
         item = self.adapt_item(body, self.viewid)
         item['isactive'] = self.isactive
         result['coordinates'] = {self.coordinates: [item]}
@@ -123,8 +134,9 @@ class ContentView(BasicView):
 
 
 class IdeasView(ContentView):
-    title = _('Ideas (${nb})')
+    title = _('Ideas')
     content_type = 'idea'
+    content_id = 'home-ideas'
     viewid = 'home-ideas'
     view_icon = 'icon novaideo-icon icon-idea'
     counter_id = 'home-ideas-counter'
@@ -133,9 +145,14 @@ class IdeasView(ContentView):
     isactive = True
 
 
+@asyn_component_config(
+    id='home-proposals',
+    on_demand=True,
+    delegate='novaideoapp_home')
 class ProposalsView(ContentView):
-    title = _('The Working Groups (${nb})')
+    title = _('The Working Groups')
     content_type = 'proposal'
+    content_id = 'home-proposals'
     viewid = 'home-proposals'
     view_icon = 'icon novaideo-icon icon-wg'
     counter_id = 'home-proposals-counter'
@@ -143,9 +160,14 @@ class ProposalsView(ContentView):
     empty_icon = 'icon novaideo-icon icon-wg'
 
 
+@asyn_component_config(
+    id='home-questions',
+    on_demand=True,
+    delegate='novaideoapp_home')
 class QuestionsView(ContentView):
-    title = _('Questions (${nb})')
+    title = _('Questions')
     content_type = 'question'
+    content_id = 'home-questions'
     viewid = 'home-questions'
     view_icon = 'md md-live-help'
     counter_id = 'home-questions-counter'
@@ -179,6 +201,7 @@ class HomeView(MultipleView):
 
     def _init_views(self, views, **kwargs):
         if self.params('load_view'):
+            delegated_by = kwargs.get('delegated_by', None)
             views = [IdeasView]
             if 'question' in self.request.content_to_manage:
                 views = [QuestionsView, IdeasView]
@@ -187,13 +210,14 @@ class HomeView(MultipleView):
                 views.append(ProposalsView)
 
             views = tuple(views)
-            if self.params('view_content_type') == 'idea':
+            view_id = self.params('view_content_id')
+            if view_id in ('home-ideas', 'pole-home-ideas'):
                 views = (IdeasView, )
 
-            if self.params('view_content_type') == 'proposal':
+            if 'home-proposals' in (delegated_by, view_id):
                 views = (ProposalsView, )
 
-            if self.params('view_content_type') == 'question':
+            if 'home-questions' in (delegated_by, view_id):
                 views = (QuestionsView, )
 
         super(HomeView, self)._init_views(views, **kwargs)

michaellaunay/KuneAgi

novaideo/views/novaideo_view_manager/home.py
@@ -42,80 +42,91 @@ class ContentView(BasicView):
     wrapper_template = 'novaideo:views/templates/simple_wrapper.pt'
     content_type = 'idea'
     isactive = False
+    hasparent=True
+    filter_class = ''
+    view_filter = [('metadata_filter', ['states', 'tree', 'challenges']), 'geographic_filter',
+                  'contribution_filter',
+                  ('temporal_filter', ['negation', 'created_date']),
+                  'text_filter', 'other_filter']
+    
+    def _get_validated(self, validated, user):
+        return validated
 
     def _add_filter(self, user):
         def source(**args):
             default_content = [self.content_type]
-            filter_ = {
-                'metadata_filter': {
-                    'content_types': default_content,
-                    'states': ['active', 'published']}
-            }
+            filter_ = self._get_validated({
+                'metadata_filter':
+                    {'content_types': default_content,
+                    'states': ['active', 'published']},
+            }, user)
             objects = find_entities(user=user, filters=[filter_], **args)
             return objects
 
         url = self.request.resource_url(
             self.context, '@@novaideoapi',
-            query={'view_content_type': self.content_type})
-        select = [('metadata_filter', ['states', 'tree', 'challenges']), 'geographic_filter',
-                  'contribution_filter',
-                  ('temporal_filter', ['negation', 'created_date']),
-                  'text_filter', 'other_filter']
+            query={'view_content_id': self.content_id})
         return get_filter(
             self,
             url=url,
             source=source,
-            select=select,
+            select=self.view_filter,
             filter_source="home",
-            filterid=self.content_type)
+            filterid=self.viewid)
 
     def update(self):
-        user = get_current()
-        filter_form, filter_data = self._add_filter(user)
-        default_content = [self.content_type]
-        validated = {
-            'metadata_filter':
-                {'content_types': default_content,
-                'states': ['active', 'published']}
-        }
-        args = {}
-        args = merge_with_filter_view(self, args)
-        args['request'] = self.request
-        objects = find_entities(
-            user=user,
-            filters=[validated],
-            **args)
-        objects, sort_body = sort_view_objects(
-            self, objects, [self.content_type], user)
-        url = self.request.resource_url(
-            self.context, '',
-            query={'view_content_type': self.content_type})
-        batch = Batch(objects,
-                      self.request,
-                      url=url,
-                      default_size=BATCH_DEFAULT_SIZE)
-        self.title = _(self.title, mapping={'nb': batch.seqlen})
-        batch.target = "#results-" + self.content_type
-        filter_instance = getattr(self, 'filter_instance', None)
-        filter_body = None
-        if filter_instance:
-            filter_data['filter_message'] = self.title
-            filter_body = filter_instance.get_body(filter_data)
-        result_body, result = render_listing_objs(
-            self.request, batch, user)
-        values = {'bodies': result_body,
-                  'batch': batch,
-                  'empty_message': self.empty_message,
-                  'empty_icon': self.empty_icon,
-                  'filter_body': filter_body,
-                  'sort_body': sort_body}
-        if filter_form:
-            result = merge_dicts(
-                {'css_links': filter_form['css_links'],
-                 'js_links': filter_form['js_links']
-                }, result)
+        body = ''
+        result = {}
+        if self.isactive or self.params('on_demand') == 'load':
+            user = get_current()
+            filter_form, filter_data = self._add_filter(user)
+            default_content = [self.content_type]
+            validated = self._get_validated({
+                'metadata_filter':
+                    {'content_types': default_content,
+                    'states': ['active', 'published']},
+            }, user)
+            args = {}
+            args = merge_with_filter_view(self, args)
+            args['request'] = self.request
+            objects = find_entities(
+                user=user,
+                filters=[validated],
+                **args)
+            objects, sort_body = sort_view_objects(
+                self, objects, [self.content_type], user)
+            url = self.request.resource_url(
+                self.context, '',
+                query={'view_content_id': self.content_id})
+            batch = Batch(objects,
+                          self.request,
+                          url=url,
+                          default_size=BATCH_DEFAULT_SIZE)
+            self.title = _(self.title, mapping={'nb': batch.seqlen})
+            batch.target = "#results-" + self.content_id
+            filter_instance = getattr(self, 'filter_instance', None)
+            filter_body = None
+            if filter_instance:
+                filter_data['filter_message'] = self.title
+                filter_body = filter_instance.get_body(filter_data)
+            result_body, result = render_listing_objs(
+                self.request, batch, user)
+            values = {'bodies': result_body,
+                      'batch': batch,
+                      'empty_message': self.empty_message,
+                      'empty_icon': self.empty_icon,
+                      'filter_body': filter_body,
+                      'filter_class': self.filter_class,
+                      'sort_body': sort_body,
+                      'view': self}
+            if filter_form:
+                result = merge_dicts(
+                    {'css_links': filter_form['css_links'],
+                     'js_links': filter_form['js_links']
+                    }, result)
 
-        body = self.content(args=values, template=self.template)['body']
+            body = self.content(args=values, template=self.template)['body']
+
         item = self.adapt_item(body, self.viewid)
         item['isactive'] = self.isactive
         result['coordinates'] = {self.coordinates: [item]}
@@ -123,8 +134,9 @@ class ContentView(BasicView):
 
 
 class IdeasView(ContentView):
-    title = _('Ideas (${nb})')
+    title = _('Ideas')
     content_type = 'idea'
+    content_id = 'home-ideas'
     viewid = 'home-ideas'
     view_icon = 'icon novaideo-icon icon-idea'
     counter_id = 'home-ideas-counter'
@@ -133,9 +145,14 @@ class IdeasView(ContentView):
     isactive = True
 
 
+@asyn_component_config(
+    id='home-proposals',
+    on_demand=True,
+    delegate='novaideoapp_home')
 class ProposalsView(ContentView):
-    title = _('The Working Groups (${nb})')
+    title = _('The Working Groups')
     content_type = 'proposal'
+    content_id = 'home-proposals'
     viewid = 'home-proposals'
     view_icon = 'icon novaideo-icon icon-wg'
     counter_id = 'home-proposals-counter'
@@ -143,9 +160,14 @@ class ProposalsView(ContentView):
     empty_icon = 'icon novaideo-icon icon-wg'
 
 
+@asyn_component_config(
+    id='home-questions',
+    on_demand=True,
+    delegate='novaideoapp_home')
 class QuestionsView(ContentView):
-    title = _('Questions (${nb})')
+    title = _('Questions')
     content_type = 'question'
+    content_id = 'home-questions'
     viewid = 'home-questions'
     view_icon = 'md md-live-help'
     counter_id = 'home-questions-counter'
@@ -179,6 +201,7 @@ class HomeView(MultipleView):
 
     def _init_views(self, views, **kwargs):
         if self.params('load_view'):
+            delegated_by = kwargs.get('delegated_by', None)
             views = [IdeasView]
             if 'question' in self.request.content_to_manage:
                 views = [QuestionsView, IdeasView]
@@ -187,13 +210,14 @@ class HomeView(MultipleView):
                 views.append(ProposalsView)
 
             views = tuple(views)
-            if self.params('view_content_type') == 'idea':
+            view_id = self.params('view_content_id')
+            if view_id in ('home-ideas', 'pole-home-ideas'):
                 views = (IdeasView, )
 
-            if self.params('view_content_type') == 'proposal':
+            if 'home-proposals' in (delegated_by, view_id):
                 views = (ProposalsView, )
 
-            if self.params('view_content_type') == 'question':
+            if 'home-questions' in (delegated_by, view_id):
                 views = (QuestionsView, )
 
         super(HomeView, self)._init_views(views, **kwargs)